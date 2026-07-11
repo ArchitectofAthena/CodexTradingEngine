fb68b9a82e2b7278f6533caa0ca293b8ef6e2371
CodenameRocky82 <coon.james82@gmail.com>
2026-07-11 15:29:48 -0500

    Expose offline control-plane validator capsule

---
+++ vendor/spiralbloom_os_v0_1/tools/validate_eve_q_contract_v0_1.py
@@ -0,0 +1,7 @@
+"""Offline CI compatibility mirror of the pinned SpiralBloom OS validator.
+
+Source commit: 792b002c95916ab1e0d1eef17a1dbf6692359fea
+Source blob: 17dd3cd77359154e3ba8f106a5cda69fd9d3b0ce
+"""
+
+from vendor.spiralbloom_os_v0_1.control_plane_capsule import *  # noqa: F403
