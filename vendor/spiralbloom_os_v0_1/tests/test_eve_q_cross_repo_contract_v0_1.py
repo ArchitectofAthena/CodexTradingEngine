bde55d0b3320b37b5a3fa95888100705236376a6
CodenameRocky82 <coon.james82@gmail.com>
2026-07-11 15:29:56 -0500

    Expose offline control-plane builder capsule

---
+++ vendor/spiralbloom_os_v0_1/tests/test_eve_q_cross_repo_contract_v0_1.py
@@ -0,0 +1,7 @@
+"""Offline CI compatibility mirror of pinned SpiralBloom OS contract builders.
+
+Source commit: 792b002c95916ab1e0d1eef17a1dbf6692359fea
+Source blob: e7979c8b4981dc0f3b25e11bf3e8ea42f3943f1d
+"""
+
+from vendor.spiralbloom_os_v0_1.control_plane_capsule import *  # noqa: F403
